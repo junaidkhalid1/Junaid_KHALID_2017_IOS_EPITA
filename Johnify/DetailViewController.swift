4bf0ae6d583ef1186feb7f16b59d31d7ed6643ea
Junaid KHALID <junaidkhalid@junaids-mac.local>
2017-02-18 19:56:18 +0100

    * Add Singer View Controller Layout done

--- Johnify/DetailViewController.swift
+++ Johnify/DetailViewController.swift
@@ -35,6 +35,9 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func btnBack(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
